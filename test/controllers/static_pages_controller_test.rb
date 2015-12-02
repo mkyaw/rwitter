dd3f6ffa54f38f4830751e51157400e19e54f08e
Myo T Kyaw <myotkyaw@gmail.com>
2015-12-02 22:08:13 +0000

    Eliminate repetition in static pages controller test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,27 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  
+  def setup
+    @basetitle = "Rwitter by Myo"
+  end
+  
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Rwitter by Myo"
+    assert_select "title", "Home | #{@basetitle}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Rwitter by Myo"
+    assert_select "title", "Help | #{@basetitle}"
   end
   
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Rwitter by Myo"
+    assert_select "title", "About | #{@basetitle}"
   end
 
 end
