26947ed6786687b32b93b8795382d422092e3bf4
Myo T Kyaw <myotkyaw@gmail.com>
2015-12-02 21:58:35 +0000

    Finish static pages

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ group :development, :test do
   gem 'sqlite3',     '1.3.9'
   gem 'web-console', '2.0.0.beta3'
   gem 'spring',      '1.1.3'
+  gem 'guard'
 end
 
 group :test do
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Rwitter by Myo"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Rwitter by Myo"
+  end
+  
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Rwitter by Myo"
   end
 
 end
