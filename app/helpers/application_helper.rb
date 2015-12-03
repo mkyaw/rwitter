64a953109ca1a3610e483c9e6f4515b89410393b
Myo T Kyaw <myotkyaw@gmail.com>
2015-12-03 03:31:19 +0000

    Set up application helper method for missing title pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    # Returns full title on pages w/o a title
+    def full_title(page_title = '')
+        base_title = "Rwitter by Myo"
+        if page_title.empty?
+            base_title
+        else
+            page_title + " | " + base_title
+        end
+    end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Rwitter by Myo"
+    assert_select "title", "Rwitter by Myo"
   end
 
   test "should get help" do
